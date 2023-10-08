1d8c4838c6ef1f6b090d974fc9d5fbf7d570adf9
Rotimi2580 <rotimi.omitoogun@gmail.com.com>
2023-10-08 12:48:46 -0700

    shell simple shell

---
+++ shell.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * main - Entry point of the program
+ * @argc:numbers of  argument
+ * @argv: array of argument vectors
+ * Return: 0 success, else failure
+ */
+
+int main(int argc, char *argv[])
+{
+	first_cmd = argv[0];
+	run_prompt = 0;
+	stats = 0;
+
+	(void)argc;
+	while (run_prompt)
+	{
+		prompt();
+
+		getln(&ln);
+
+		cmd = str_to_arr(ln, ' ');
+
+		stats = exe_cmd();
+
+		free(ln);
+		freeArr(cmd);
+	}
+
+	return (stats);
+}
---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*List of standard library function used*/
+#include <stdio.h>
+#include <unistd.h>
+
+/*User defined functions*/
+
+
+/*Global variables used*/
+char *ln;
+int run_cmd;
+
+#endif /*SHELL_H*/

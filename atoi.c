3b49a63211981a84c478321fc05af2cea3ac5ff8
Rotimi2580 <rotimi.omitoogun@gmail.com.com>
2023-10-09 14:34:13 -0700

    atoi function

---
+++ atoi.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * _atoi -  converts a string to integers
+ * @s: string to converted
+ *
+ * Return: the integer
+ */
+int _atoi(const char *s)
+{
+	int res = 0;
+	int zign = 1;
+	int k = 0;
+
+	if (s[0] == '-')
+	{
+		zign = -1;
+		k++;
+	}
+	while (s[k] != '\0')
+	{
+		if (s[k] < '0' || s[k] > '9')
+			return (-2);
+		result = res * 10 + (s[k] - '0');
+		k++;
+	}
+	return (zign * res);
+}

93a9b13ef48d170e807d7c3a7a2372e1af02d668
elmostakun <elmostapha.dounia@gmail.com>
2023-10-09 08:45:42 -0700

    fix

--- shell.h
+++ shell.h
@@ -10,6 +10,12 @@
 
 /*User defined functions*/
 int prompt(void);
+ssize_t take_line(char **ln);
+char *_cat(char *path, char *stpth);
+char *_cpy(char *path, char *stpth);
+char *_dup(char *string);
+int _compare(char *string1, char *string2);
+int _leng(char *string);
 
 /*Global variables used*/
 char **cmd;
---
+++ take_line.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+/**
+ * take_ln - have string from user and stores in a buffer
+ * @ln: buffer for the string to be stored
+ *
+ * Return: number of characters read, if errror returns -1;
+ */
+ssize_t take_line(char **ln)
+{
+char *bff = _calloc(12144, sizeof(char));
+ssize_t idx = 0;
+
+
+while ((read(0, &bff[idx], 1)) > 0 && (bff[idx] != '\n'))
+idx++;
+
+if (bff[idx] == '\n')
+{
+*ln = malloc(sizeof(char) * (idx + 1));
+bff[idx] = '\0';
+_cpy(*ln, bff);
+free(bff);
+return (idx);
+}
+else if (idx != 0)
+{
+bff[idx] = '\0';
+*ln = malloc(sizeof(char) * (idx + 1));
+_cpy(*ln, bff);
+free(bff);
+return (-1);
+}
+else
+{
+if (isatty(0))
+_printssf(1, "\n", "nill", 0, "nill");
+free(bff);
+exit(stat_s);
+}
+}

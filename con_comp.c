f73ff71b150e788d0de58abf0844cbb187082164
elmostakun <elmostapha.dounia@gmail.com>
2023-10-13 01:58:50 -0700

    check con comp

--- con_comp.c
+++ con_comp.c
@@ -40,26 +40,3 @@ free(bff);
 
 return (array);
 }
-
-/**
- * c_s - compares 2 strings
- * @string1: first string
- * @string2: second string compared on/with first string
- * @num: number of string to be compared
- * Return: 0 if equal, positive if s1 < s2, negative if s1 > s2
- */
-
-int c_s(const char *string1, const char *string2, size_t num)
-{
-size_t idx;
-
-for (idx = 0; idx < num; idx++)
-{
-if (string1[idx] == '\0' || string2[idx] == '\0')
-return (string1[idx] - string2[idx]);
-
-if (string1[idx] != string2[idx])
-return (string1[idx] - string2[idx]);
-}
-return (0);
-}
